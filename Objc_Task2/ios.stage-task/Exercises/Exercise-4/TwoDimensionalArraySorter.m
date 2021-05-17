88c7c81271f8da597f352d74687d4acf4766fb0d
Veronika <Veronikasazonova210@gmail.com>
2021-05-17 21:43:37 +0300

    feat: ex.4 add

--- Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
@@ -3,7 +3,26 @@
 @implementation TwoDimensionalArraySorter
 
 - (NSArray *)twoDimensionalSort:(NSArray<NSArray *> *)array {
-    return @[];
+    if (array == nil){return @[];}
+    
+    for (NSArray *subArray in array){
+        if (![subArray isKindOfClass:[NSArray class]]) return @[];
+    }
+    
+    NSMutableArray *newArray = [[NSMutableArray alloc] init];
+    
+    for (int i = 0; i < [array count]; i++) {
+        for (int j = 0; j < [array[i] count]; j++) {
+            [newArray addObject: array[i][j]];
+        }
+    }
+    
+    NSSortDescriptor *sortDescriptor;
+    sortDescriptor = [[NSSortDescriptor alloc] initWithKey:nil ascending:YES];
+    NSArray *sortDescriptors = [NSArray arrayWithObject:sortDescriptor];
+    NSArray *sortedArray = [newArray sortedArrayUsingDescriptors:sortDescriptors];
+    
+    return sortedArray;
 }
 
 @end
