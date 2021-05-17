9cdc40e9646597d6898a152eeae325c7b16b337e
Veronika <Veronikasazonova210@gmail.com>
2021-05-17 21:32:39 +0300

    feat: ex.1 add

--- Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
@@ -1,5 +1,13 @@
 #import "ReverseBinary.h"
 
 UInt8 ReverseInteger(UInt8 n) {
-    return 0;
+    int m = 0;
+    
+        for (int i = 0; i < 8; i++) {
+            m <<= 1;
+            m |= n&1;
+            n >>= 1;
+        }
+
+    return m;
 }
